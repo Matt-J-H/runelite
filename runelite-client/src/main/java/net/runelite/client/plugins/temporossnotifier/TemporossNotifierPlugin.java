cd0f544baceb31f2063c021bbebb49e153d0ad1b
Matt Hietpas <mjhietpas@gmail.com>
2021-10-11 17:04:46 -0500

    Update TemporossNotifierPlugin.java

--- runelite-client/src/main/java/net/runelite/client/plugins/temporossnotifier/TemporossNotifierPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/temporossnotifier/TemporossNotifierPlugin.java
@@ -1,5 +1,40 @@
 package net.runelite.client.plugins.temporossnotifier;
 
-public class TemporossNotifierPlugin {
-    
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.NPC;
+import net.runelite.api.NpcID;
+import net.runelite.api.events.NpcSpawned;
+import net.runelite.client.Notifier;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.game.FishingSpot;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.Plugin;
+
+import javax.inject.Inject;
+
+@PluginDescriptor(
+        name = "Tempoross Notifier",
+        description = "Notifies you when a double Harpoonfish spot appears."
+)
+@Slf4j
+public class TemporossNotifierPlugin extends Plugin
+{
+    @Inject
+    private Notifier notifier;
+
+    @Subscribe
+    public void onNpcSpawned(NpcSpawned event)
+    {
+        final NPC npc = event.getNpc();
+        FishingSpot spot = FishingSpot.findSpot(npc.getId());
+        if (spot == null)
+        {
+            return;
+        }
+
+        if (spot == FishingSpot.HARPOONFISH && npc.getId() == NpcID.FISHING_SPOT_10569)
+        {
+            notifier.notify("A double Harpoonfish spot has appeared.");
+        }
+    }
 }
